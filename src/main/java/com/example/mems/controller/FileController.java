343e6fe093b4fa04ac87868a07b4e61ede6dcc7b
敢抄我的码 <78305741+Anti-Involution@users.noreply.github.com>
2022-06-20 01:23:32 +0800

    file update

--- src/main/java/com/example/mems/controller/FileController.java
+++ src/main/java/com/example/mems/controller/FileController.java
@@ -1,9 +1,13 @@
 package com.example.mems.controller;
 
 
+import com.example.mems.entity.File;
+import com.example.mems.mapper.FileMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/file")
 public class FileController {
+    @Autowired
+    private FileMapper filemapper;
+    @RequestMapping("/queryFileSyn0List")
+    public List<File> queryFileSyn0List(){
+        List<File> filelist=filemapper.querySyn0List();
+        for(File file:filelist){
+            System.out.println(file);
+        }
+        return filelist;
+    }
 
-}
+    }
+
+
+
 
--- src/main/java/com/example/mems/mapper/FileMapper.java
+++ src/main/java/com/example/mems/mapper/FileMapper.java
@@ -1,8 +1,12 @@
 package com.example.mems.mapper;
 
-import com.example.mems.entity.File;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mems.entity.File;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+@Mapper
 /**
  * <p>
  *  Mapper 接口
@@ -12,5 +16,10 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2022-06-19
  */
 public interface FileMapper extends BaseMapper<File> {
+    List<File> queryFileList();
+    List<File> querySyn0List();
+    int addFile(File file);
+    int updateFile(File file);
+    int deleteFile(int id);
 
 }
